ae479a1c2501ecf0a58760c2ada301ea3601b7be
trueryk <henrique.jf01@gmail.com>
2026-08-24 11:20:56 -0300

    backend atualizado

--- src/main/java/com/biblioteca/backend/model/exemplar.java
+++ src/main/java/com/biblioteca/backend/model/exemplar.java
@@ -1,13 +1,12 @@
 package com.biblioteca.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Getter
 @Setter
@@ -19,5 +18,11 @@ public class exemplar{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String codigo_exemplar;
     private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "livro_id")
+    private livro livro;
+
 }
